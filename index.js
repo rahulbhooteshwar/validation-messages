bf21490d5306242b3054f1b5b6578781adbe0b2d
Rahul Bhooteshwar <rahul.bhooteshwar@mastercard.com>
2017-07-28 08:08:06 +0530

    Cleanup for fresh repo.

--- index.js
+++
@@ -1,2 +0,0 @@
-exports.MessageConfigService = require('./lib/').MessageConfigService;
-exports.ValidationMessagesDirective = require('./lib/').ValidationMessagesDirective;
--- src/validation-messages/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './message-config.service';
-export * from './validation-messages.directive';
-export * from './validation-messages.module';
